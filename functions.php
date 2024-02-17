ef60b07d5a1c6037b231b0a0d5a92af1e0753145
Kurt D-S <delos.santos.kurt.allen@gmail.com>
2024-02-17 15:11:36 +0800

    feat: implement regex for dynamic routing

--- functions.php
+++ functions.php
@@ -17,25 +17,48 @@ function run(string $url, array $routes): void
 {
     $uri = parse_url("$url");
     $path = $uri['path'];
-
     $path_array = explode('/', $path);
-    $path = '/' . $path_array[1];
-    $id = 0;
+
+    $regex_bookID_reviewID = "/\/books\/\d+\/reviews\/\d+/";
+    $regex_bookID_review = "/\/books\/\d+\/reviews/";
+    $regex_bookID_edit = "/\/books\/\d+\/edit/";
+    $regex_bookID = "/\/books\/\d+/";
+    $bookID = 0;
+    $reviewID = 0;
     $path2 = '';
-    if (count($path_array) === 3) {
-        if ($path_array[2] === 'new') {
-            $path .= '/new';
-        } else {
-            $id = $path_array[2];
-        }
-    } else if (count($path_array) === 4) {
-        $id = $path_array[2];
-        if ($path_array[3] === 'edit') {
-            $path2 = 'edit';
-        }
+
+    if (preg_match($regex_bookID_reviewID, $path, $matches) && count($path_array) === 5) {
+        $path = '/' . $path_array[1];
+        $bookID = $path_array[2];
+        $reviewID = $path_array[4];
+        print_r($matches);
+        echo "<br>";
+        echo("SHOWING /books/:id/reviews/:id URL");
+        echo "<br>";
+        echo "BOOK ID:" . $bookID;
+        echo "<br>";
+        echo "REVIEW ID: " . $reviewID;
+        die();
+    } else if ((preg_match($regex_bookID_review, $path, $matches) && count($path_array) === 4)) {
+        $path = '/' . $path_array[1];
+        $bookID = $path_array[2];
+        print_r($matches);
+        echo "<br>";
+        echo("SHOWING /books/:id/reviews/:id URL");
+        echo "<br>";
+        echo "BOOK ID:" . $bookID;
+        die();
+    } else if ((preg_match($regex_bookID_edit, $path, $matches) && count($path_array) === 4)) {
+        $path = '/' . $path_array[1];
+        $bookID = $path_array[2];
+        $path2 = 'edit';
+    } else if ((preg_match($regex_bookID, $path, $matches) && count($path_array) === 3)) {
+        $path = '/' . $path_array[1];
+        $bookID = $path_array[2];
     }
+
     
-    if (false === array_key_exists($path, $routes) || (count($path_array) > 3 && $path2 !== 'edit') ) {
+    if (false === array_key_exists($path, $routes)) {
         http_response_code(404);
         echo "404-NOT FOUND";
         return;
@@ -46,7 +69,7 @@ function run(string $url, array $routes): void
     if (!empty($uri['query'])){
         parse_str($uri['query'], $params);
     }
-    $params['book_id'] = $id;
+    $params['book_id'] = $bookID;
     $params['path2'] = $path2;
     $callback($params);
 }
