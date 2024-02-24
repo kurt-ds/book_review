60d75b24933fa19cbdafd90f7157b8b8d75de69c
Christine Filomeno <124708598+pabolatte@users.noreply.github.com>
2024-02-25 06:14:14 +0800

    signup.view

--- views/signup.view.php
+++ views/signup.view.php
@@ -1,22 +1,30 @@
 <?php require 'partials/head.php' ?>
+<?php require 'partials/header.php' ?>
 <?php require 'partials/show_signup_errors.php'?>
 
-<p>This is where you sign up</p>
-
-<h3>SIGN UP FORM</h3>
-<form action="/signup" method='post'>
-    <input type="text" name='username' id='username' placeholder='Username'>
-    <br>
-    <input type="text" name='firstname' id='firstname' placeholder='First name'>
-    <br>
-    <input type="text" name='lastname' id='lastname' placeholder='Last Name'>
-    <br>
-    <input type="password" name='pwd' id='pwd' placeholder='Password'>
-    <br>
-    <input type="email" name='email' id='email' placeholder='Email'>
-    <br>
-    <button type='submit' >Sign up</button>
+<div class = "sign-up_page"> 
+<div class="sign-up_container">
+        <h3>CREATE ACCOUNT</h3>
+        
+        <form action="/signup" method='post'>
+        <div class="sign-up_inputs">
+          <div class="sign-up-name_inputs">
+            <input type="text" name='firstname' id='firstname' placeholder='First name' required>
+            <input type="text" name='lastname' id='lastname' placeholder='Last Name' required>
+          </div>
+          <input type="text" name='username' id='username' placeholder='Username' required>
+          <input type="password" name='pwd' id='pwd' placeholder='Password' required>
+          <input type="email" name='email' id='email' placeholder='Email' require>
+        </div>
+      
+        <button type ="submit" class="sign-up_button">Create Account</button>
+      
+        <p>Already have an account? <a href="/login" class="login-link">Login</a></p>
+      
+      </div>
 </form>
+</div>
+</div>
 
 <?php  check_signup_errors(); ?>
 
